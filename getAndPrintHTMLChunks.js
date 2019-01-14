f55c5bc5144478675b8943578f876771d247ca98
Jason <jason.jiron@gmail.com>
2019-01-14 20:38:12 +0000

    added '\n' to better display console.log()

--- getAndPrintHTMLChunks.js
+++ getAndPrintHTMLChunks.js
@@ -9,14 +9,14 @@ function getAndPrintHTMLChunks (callback) {
   };
 
   /* Add your code here */
-  https.get('https://api.github.com', function(response) {
+  https.get(requestOptions, function(response) {
 
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
   
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      console.log('Chunk Received. Length:', data.length);
+      console.log(data + '\n');
     });
   
     // the callback is invoked when all of the data has been received
