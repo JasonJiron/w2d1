815b7935b60ffab646ee6c62b4fc6f9d925800ae
Jason <jason.jiron@gmail.com>
2019-01-15 02:05:08 +0000

    learning how to use npm request

--- node-request/server.js
+++ node-request/server.js
@@ -1,6 +1,16 @@
 var request = require('request');
+var fs = require('fs');
 
-request('https://sytantris.github.io/http-examples/', function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode);
-});
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function(err) {
+    throw err
+  })
+  .on('data', function(res) {
+    console.log('Downloading image...');
+  })
+  .on('resonse', function(res) {
+    console.log('Download complete.');
+  })
+  .pipe(fs.createWriteStream('./future.jpg'))
+ 
